e46a681f3b71ced1df05ee88abf61474bcf5e7a2
Cecilie <cecilie@brandt.net>
2021-05-24 18:24:48 +0200

    edit test route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PostsComponent } from './components/posts/posts.component';
 import { NewpostComponent } from './components/newpost/newpost.component';
+import { EditpostComponent } from './components/editpost/editpost.component';
 
 const routes: Routes = [
   { path: 'posts', component: PostsComponent },
-  { path: 'new-post', component: NewpostComponent }
+  { path: 'new-post', component: NewpostComponent },
+  { path: 'edit-post', component: EditpostComponent }
 ];
 
 @NgModule({
