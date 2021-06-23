bd5746a93455d81c851b4219cd2eecd4532e526e
Michael Fuglø <michael@fuglo.com>
2021-06-23 17:56:46 +0200

    setup collections page

--- src/app/components/collections/collections.component.ts
+++ src/app/components/collections/collections.component.ts
@@ -1,4 +1,10 @@
+import { NgRedux } from '@angular-redux/store';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Collection } from '../../entities/Collection';
+import { CollectionActions } from '../../store/actions/CollectionActions';
+import { AppState, PostState } from '../../store/Store';
+import { EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-collections',
@@ -6,10 +12,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./collections.component.scss']
 })
 export class CollectionsComponent implements OnInit {
+  search = '';
+  collections!: Collection[];
+  collectionClicked: EventEmitter<any> = new EventEmitter<any>();
 
-  constructor() { }
+  displayedColumns: string[] = ['title', 'created', 'contents', 'status', 'edit'];
+
+  constructor(
+    private router: Router,
+    private collectionActions: CollectionActions,
+    private ngRedux: NgRedux<AppState>
+  ) {
+  }
 
   ngOnInit(): void {
+    this.ngRedux
+      .select((state) => state.collections)
+      .subscribe((res) => {
+        res?.collections ? (this.collections = res.collections) : (this.collections = []);
+      });
+  }
+
+  editCollection(id: any) {
+    this.router.navigate(['edit-collection', {myId: id}])
   }
 
 }
--- src/app/entities/Collection.ts
+++ src/app/entities/Collection.ts
@@ -1,6 +1,7 @@
 export interface Collection {
   id?: string;
   title: string;
+  createdAt: string;
   description: string;
   contentIds?: string[] | null;
   published: boolean;
